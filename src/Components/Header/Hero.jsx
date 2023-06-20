a213d5dcab83d5f606d4979e34a1b31a0e22fa68
P-1000 <pavan.patchikarla@yahoo.com>
2023-06-21 02:52:08 +0530

    render

--- src/Components/Header/Hero.jsx
+++ src/Components/Header/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <div className="relative bg-[#add6ef]">
       <div className="absolute top-0 left-0 w-full h-full">
         <div className="-rotate-90 absolute top-[47px] left-1/4 transform -translate-x-1/2 -translate-y-1/2">
-          <img src="../public/landingpage/e2.png" alt="Half Circle" className="w-auto h-72" />
+          <img src="landingpage/e2.png" alt="Half Circle" className="w-auto h-72" />
         </div>
       </div>
       <div className="container mx-auto py-10 lg:py-14 relative">
@@ -21,7 +21,7 @@ const Hero = () => {
         <div className="flex justify-center mt-4">
           <div className="lg:w-[28rem] lg:h-[28rem] w-[24rem] h-[24rem] bg-[#94bae3] rounded-full">
             <img
-              src="../public/landingpage/hero.png"
+              src="landingpage/hero.png"
               alt="team"
               className="mt-10 w-full h-full object-contain"
             />
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
         <div className="absolute hidden lg:block -top-10 -right-28 w-auto h-screen flex justify-end items-center">
           <div className="rotate-270 ml-96">
-            <img src="../public/landingpage/e2.png" alt="Half Circle" className="w-auto h-[calc(100vh - 100px)]" />
+            <img src="landingpage/e2.png" alt="Half Circle" className="w-auto h-[calc(100vh - 100px)]" />
           </div>
         </div>
       </div>
@@ -38,7 +38,7 @@ const Hero = () => {
       <div className=" mt-10">
         <div className="absolute  lg:top-[65%] z-50 left-0 transform -translate-y-1/2">
           <div className="-rotate-180">
-            <img src="../public/landingpage/e2.png" alt="Half Circle" className="w-auto h-[10rem] lg:h-[30rem]" />
+            <img src="landingpage/e2.png" alt="Half Circle" className="w-auto h-[10rem] lg:h-[30rem]" />
           </div>
         </div>
         <div className="bg-[#3c56b8] py-20">
